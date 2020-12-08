96ed2c394c9a0e23be1fbb571200405e36c2222f
Hadleigh-Schwartz <hadleigh.schwartz@gmail.com>
2020-12-08 10:04:57 -0500

    scene transitions and state changes are solid

---
+++ MVP/Assets/Scripts/PlayerKinematicClouds.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerKinematicClouds : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    public float speed;
+
+    Vector2 direction;
+
+    public bool isEvaporating = false;
+    public bool isCondensing = false;
+
+
+    Vector2 startEvapPos, endEvapPos;
+    Vector2 startCondPos, endCondPos;
+
+    float currentLerpTime;
+    float lerpTime = 2f;
+
+    public StateManager gameManager;
+
+ 
+     
+    // private SphereCollider col;
+
+    // private Level2Behavior gameManager;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        // col = GetComponent<SphereCollider>();
+
+        gameManager = GameObject.Find("GameManager").GetComponent<StateManager>();
+
+    
+    }
+
+    void Update()
+    {
+        direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    void FixedUpdate(){
+        rb.MovePosition((Vector2)transform.position + (direction * speed * Time.fixedDeltaTime));
+
+
+        if (isEvaporating)
+         {
+             //increment timer once per frame
+             currentLerpTime += Time.deltaTime;
+             if (currentLerpTime > lerpTime)
+             {
+                 currentLerpTime = lerpTime;
+                 isEvaporating = false;
+             }
+ 
+             //lerp
+             float percentComplete = currentLerpTime / lerpTime;
+             rb.MovePosition(Vector3.Lerp(startEvapPos, endEvapPos, percentComplete));
+         }
+
+         if (isCondensing)
+         {
+             //increment timer once per frame
+             currentLerpTime += Time.deltaTime;
+             if (currentLerpTime > lerpTime)
+             {
+                 currentLerpTime = lerpTime;
+                 isCondensing = false;
+             }
+ 
+             //lerp
+             float percentComplete = currentLerpTime / lerpTime;
+             rb.MovePosition(Vector3.Lerp(startCondPos, endCondPos, percentComplete));
+         }
+        
+
+
+    }
+
+
+    public void startCondense(){
+        if (gameManager.state != "gas"){
+            Debug.Log("Error!");
+            gameManager.showErrorScreen = true;
+        }
+        else
+        {
+            gameManager.state = "liquid";
+            startCondPos = transform.position;
+            endCondPos = new Vector2(transform.position.x, -10);
+            isCondensing = true;
+            currentLerpTime = 0f; 
+        }
+       
+    }
+
+
+    public void startEvaporate(){
+        if (gameManager.state != "liquid"){
+            Debug.Log("Error!");
+            gameManager.showErrorScreen = true;
+        }
+        else
+        {
+            gameManager.state = "gas";
+            startEvapPos = transform.position;
+            endEvapPos = new Vector2(transform.position.x, 18);
+            isEvaporating = true;
+            currentLerpTime = 0f; 
+        }
+       
+    }
+
+
+
+}
--- MVP/Assets/Scripts/SceneManagement.cs
+++ MVP/Assets/Scripts/SceneManagement.cs
@@ -7,6 +7,14 @@ public class SceneManagement : MonoBehaviour {
  
     public void startLevel2() {
     	Debug.Log("entered start level2");
-        SceneManager.LoadScene("level2-cloud-intro");      
+        SceneManager.LoadScene("level2-cloud-intro");     
+        SceneManager.UnloadSceneAsync("Home");
+ 
+    }
+
+    public void returnHome() {
+        SceneManager.LoadScene("Home");  
+        SceneManager.UnloadSceneAsync("level2-cloud-intro");
+    
     }
 }
