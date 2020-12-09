7cf2b7d2f3f7bb8d3d7c9190e929e6a7e77a92c8
lukellie pc <58856900+lukellie@users.noreply.github.com>
2020-12-09 12:46:39 -0600

    Dialogue system working, with multiple paragraphs available

--- MVP/Assets/Scripts/SceneManagement.cs
+++ MVP/Assets/Scripts/SceneManagement.cs
@@ -2,19 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
- 
+using TMPro;
+using UnityEngine.UI;
+
 public class SceneManagement : MonoBehaviour {
- 
+
     public void startLevel2() {
     	Debug.Log("entered start level2");
-        SceneManager.LoadScene("level2-cloud-intro");     
+        SceneManager.LoadScene("level2-cloud-intro");
         SceneManager.UnloadSceneAsync("Home");
- 
+
     }
 
     public void returnHome() {
-        SceneManager.LoadScene("Home");  
+        SceneManager.LoadScene("Home");
         SceneManager.UnloadSceneAsync("level2-cloud-intro");
-    
+
     }
 }
