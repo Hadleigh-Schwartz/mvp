590cbcf9c938e47a6e835a554be01b27242664a1
deButts <eadebutts@gmail.com>
2020-12-08 21:45:45 -0500

    Added inventory GUI not sure what to attach it too

---
+++ MVP/Assets/Scripts/PollutionGUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PollutionGUI : MonoBehaviour
+{
+    public Button yourButton;
+    public bool showPollutionGUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+        showPollutionGUI = false;
+    }
+    void TaskOnClick()
+    {
+        Debug.Log("You have clicked the pollution button!");
+        //set our bool to true
+        showPollutionGUI = !showPollutionGUI;
+    }
+    private void OnGUI()
+    {
+      if(showPollutionGUI)
+      {
+        print("You have picked up clean water");
+      }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
