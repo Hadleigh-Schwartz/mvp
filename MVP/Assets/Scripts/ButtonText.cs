20f73e678fc5058b315cc04cf4aefdfced9f842a
deButts <eadebutts@gmail.com>
2020-12-09 14:12:05 -0500

    made button that shows and removes text from ui

---
+++ MVP/Assets/Scripts/ButtonText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonText : MonoBehaviour
+{
+    public Text textField;
+    public void SetText(string txt)
+    {
+      if(textField.text == "")
+      {
+        textField.text = txt;
+      }
+      else
+      {
+        textField.text = "";
+      }
+    }
+}
