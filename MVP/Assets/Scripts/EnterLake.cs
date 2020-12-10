e8366b39f14515b2afffab8166882014ce7fd3e4
deButts <eadebutts@gmail.com>
2020-12-10 11:21:33 -0500

    level 3 done

--- MVP/Assets/Scripts/EnterLake.cs
+++ MVP/Assets/Scripts/EnterLake.cs
@@ -2,19 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
- 
+
 public class EnterLake : MonoBehaviour {
- 
+
     void OnTriggerEnter2D(Collider2D other) {
     	Debug.Log("entered");
     	Debug.Log(other);
-        
+
         Debug.Log("success");
         if (other.gameObject.name == "Player")
       {
-        SceneManager.LoadScene("level3");
+        SceneManager.LoadScene("level3-intro");
       }
-        
-        
+
+
     }
 }
---
+++ MVP/Assets/Scripts/EnterLevel3.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnterLevel3 : MonoBehaviour
+{
+    public void StartChallenge()
+    {
+      SceneManager.LoadScene("level3");
+    }
+}
